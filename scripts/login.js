494a6f233919179717d9e70a9da02404a12655fc
Boymeash29 <boymeash29@7hills.org>
2024-12-17 13:28:37 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,9 @@
 function check(form) {
-    username = new Array("admin1", "admin2", "members");
-    password = new Array("admin1", "admin2", "members");
+    username = new Array("Asher", "Muhammed", "Theo");
+    password = new Array(")bpG!5Ls$w04", "&Ji+)1221a47", ",xzH46@S@30B");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
-		location.replace('#adminhome')/* The Administrator homepage */
+		location.replace('memebers/home.html')/* The Administrator homepage */
 	}
 	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
 		location.replace('memebers/home.html')/* The members homepage */
