e0683087de2df31298ed6e2f318190e95135f6bf
Boymeash29 <boymeash29@7hills.org>
2024-12-18 17:57:19 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
 function check(form) {
-    let username = ["Asher", "Muhammad", "Theo"];
-    let password = [")bpG!5Ls$w04", "&Ji+)1221a47", "xzH46@S@30B"];
+    let username = ["Asher", "Muhammad", "Theo", "Admin"];
+    let password = [")bpG!5Ls$w04", "&Ji+)1221a47", "xzH46@S@30B", "25ib[{cN9'|A"];
 
     for (let i = 0; i < username.length; i++) {
         if (form.userid.value === username[i] && form.pswrd.value === password[i]) {
@@ -9,7 +9,7 @@ function check(form) {
             localStorage.setItem("username", username[i]); // Store the logged-in username
 
             // Redirect based on user role
-            if (username[i] === "Asher" || username[i] === "Muhammad") {
+            if (username[i] === "Asher" || username[i] === "Muhammad" || username[i] === "Admin") {
                 location.replace("admin.html"); // Redirect admins to admin panel
             } else {
                 location.replace("memebers/home.html"); // Redirect regular users to home
